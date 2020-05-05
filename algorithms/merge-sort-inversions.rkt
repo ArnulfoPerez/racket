6a618763f9cd00dfb93f7a0ee5b832fa571a8bf2
ArnulfoPerez <arnulfo.perez.p@gmail.com>
2020-05-05 12:16:36 -0500

    working merge merge-sort-inversions.rkt

--- algorithms/merge-sort-inversions.rkt
+++ algorithms/merge-sort-inversions.rkt
@@ -10,7 +10,7 @@
            [((list a as ...) (list b bs ...))
             (if (< a b)
                 (let-values ([(lst count)(aux as (cons b bs) inversions)]) (values (cons a lst) (+ inversions count)))
-                (let-values ([(lst count)(aux (cons a as) bs inversions)]) (values (cons b lst) (+ inversions count))))]))])
+                (let-values ([(lst count)(aux (cons a as) bs inversions)]) (values (cons b lst) (+ inversions count (length as) 1))))]))])
        (aux as bs 0)))
 
 (define (merge-sort vs)
