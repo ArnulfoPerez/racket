12cce06e1d349f880f5fe55ef9e494a81b8f4945
ArnulfoPerez <arnulfo.perez.p@gmail.com>
2020-05-05 11:21:51 -0500

    initial incomplete merge-sort-inversions.rkt

---
+++ algorithms/merge-sort-inversions.rkt
@@ -0,0 +1,20 @@
+#lang racket
+(define (merge as bs)
+  (let
+      ((define aux (as bs inversions)
+         (match* (as bs)
+           [((list) bs) bs]
+           [(as (list)) as]
+           [((list a as ...) (list b bs ...))
+            (if (< a b)
+                (cons a (merge as (cons b bs)))
+                (cons b (merge (cons a as) bs)))]))
+       (aux as bs 0))))
+
+(define (merge-sort vs)
+  (match vs
+    [(list) vs]
+    [(list a) vs]
+    [_ (define-values (lvs rvs)
+         (split-at vs (quotient (length vs) 2)))
+       (merge (merge-sort lvs) (merge-sort rvs))]))
